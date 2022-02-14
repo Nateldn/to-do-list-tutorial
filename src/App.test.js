164f7d900830a9f90cedec1c6be7f397af42fd0c
Nateldn <32700665+Nateldn@users.noreply.github.com>
2022-02-14 10:21:36 +0000

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/to-do list/i);
+  expect(element).toBeInTheDocument();
 });
+test('renders header', () => {
+  render(<App />);
+  const element = screen.getByRole("textbox");
+  expect(element).toBeInTheDocument();
+});
+describe("submit button", () => {
+  test("submit button is in the document", () => {
+    render(<App />)
+    const button = screen.getByText("submit")
+    expect(button).toBeInTheDocument()
+  })
+  test("submit button is in the document", () => {
+    render(<App />)
+    const button = screen.getByText("submit")
+    expect(button).toBeInTheDocument()
+  })
+})
